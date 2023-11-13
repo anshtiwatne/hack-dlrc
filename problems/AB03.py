f90b7fb9cb158701ccaf59a6651f737be43ea66b
Aneesh <aneesh1701@gmail.com>
2023-11-13 16:46:02 +0530

    balanced number of resets for AB03 generator

--- problems/AB03.py
+++ problems/AB03.py
@@ -3,7 +3,7 @@ from random import random, randint
 for _ in range(1024):  # we can go wild with this one
     p = random()
 
-    if p < 0.90:  # 90% chance it is a direction
+    if p < 0.95:  # 95% chance it is a direction
         print("NEWS"[randint(0, 3)])
-    else:  # 10% chance it is a reset
+    else:  # 5% chance it is a reset
         print("R")

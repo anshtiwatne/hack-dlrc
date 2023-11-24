c872395d74c3ea614bd182e6ce2e406806b755eb
Ansh Tiwatne <ansh.tiwatne@gmail.com>
2023-11-24 15:13:23 +0530

    added json dump

--- private/problems/AT02.py
+++ private/problems/AT02.py
@@ -1,4 +1,5 @@
 import random
+import json
 from pprint import pprint
 
 NUCLEOTIDES = ['A', 'C', 'G', 'T']
@@ -15,25 +16,21 @@ def find_mutation(main, complementary):
 
 def get_data():
     """returns input and corresponding output data"""
-    data = {i: {} for i in range(16)}
 
-    for i in range(16):
-        # Generate two random strands of DNA
-        main_strand = [random.choice(NUCLEOTIDES) for _ in range(1024)]
-        complementary_strand = [PAIRS[n] for n in main_strand]
+    # Generate two random strands of DNA
+    main_strand = [random.choice(NUCLEOTIDES) for _ in range(1024)]
+    complementary_strand = [PAIRS[n] for n in main_strand]
 
-        # Mutate one nucleotide
-        replacement = random.choice(range(len(main_strand)))
-        mutation = random.choice(['C', 'G']) if main_strand[replacement] == "A" else random.choice(['A', 'T'])
-        main_strand[replacement] = mutation
+    # Mutate one nucleotide
+    replacement = random.choice(range(len(main_strand)))
+    mutation = random.choice(['C', 'G']) if main_strand[replacement] == "A" else random.choice(['A', 'T'])
+    main_strand[replacement] = mutation
 
-        data[i] = {
-            'inp': '\n\n'.join([''.join(main_strand), ''.join(complementary_strand)]),
-            'out': find_mutation(main_strand, complementary_strand)
-        }
+    return json.dumps({
+        'problem': '\n\n'.join([''.join(main_strand), ''.join(complementary_strand)]),
+        'solution': find_mutation(main_strand, complementary_strand)
+    })
 
-    return data
 
 if __name__ == '__main__':
     pprint(get_data())
-
